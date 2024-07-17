92e4a256db9dc0462ce54677b3da6225e0fc0d64
NikolayD <volodar86@yandex.ru>
2024-07-17 15:43:27 +0300

    make private method to parse UIColor to rgb components and set them to sliders. in IBAction use delegate to set background color of MainViewController.

--- RGBView/ViewControllers/ColorSettingViewController.swift
+++ RGBView/ViewControllers/ColorSettingViewController.swift
@@ -19,17 +19,20 @@ final class ColorSettingViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    var mainViewControllerColor: UIImage!
+    var mainVCBackgroundColor: UIColor!
+    weak var delegate: ColorSettingViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         colourView.layer.cornerRadius = 30
         
+        setSlidersValue(from: mainVCBackgroundColor)
+        
         setViewColour()
-        setValue(redSlider.value, ForLabel: redLabel)
-        setValue(greenSlider.value, ForLabel: greenLabel)
-        setValue(blueSlider.value, ForLabel: blueLabel)
+        setValue(redSlider.value, forLabel: redLabel)
+        setValue(greenSlider.value, forLabel: greenLabel)
+        setValue(blueSlider.value, forLabel: blueLabel)
     }
     
     @IBAction func sliderValueChanged(_ sender: UISlider) {
@@ -37,13 +40,28 @@ final class ColorSettingViewController: UIViewController {
         
         switch sender {
         case redSlider:
-            setValue(redSlider.value, ForLabel: redLabel)
+            setValue(redSlider.value, forLabel: redLabel)
         case greenSlider:
-            setValue(greenSlider.value, ForLabel: greenLabel)
+            setValue(greenSlider.value, forLabel: greenLabel)
         default:
-            setValue(blueSlider.value, ForLabel: blueLabel)
+            setValue(blueSlider.value, forLabel: blueLabel)
         }
     }
+    
+    @IBAction func doneButtonTapped() {
+        if let delegate = delegate {
+            delegate.setBackGroundColor(
+                UIColor(
+                    red: CGFloat(redSlider.value),
+                    green: CGFloat(greenSlider.value),
+                    blue: CGFloat(blueSlider.value),
+                    alpha: 1
+                )
+            )
+        }
+        dismiss(animated: true)
+    }
+    
     
     private func setViewColour() {
         colourView.backgroundColor = UIColor(
@@ -54,10 +72,22 @@ final class ColorSettingViewController: UIViewController {
         )
     }
     
-    private func setValue(_ value: Float, ForLabel label: UILabel) {
+    private func setValue(_ value: Float, forLabel label: UILabel) {
         label.text = value.formatted(
             .number.precision(.fractionLength(2))
         )
+    }
+    
+    private func setSlidersValue(from color: UIColor) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        redSlider.value = Float(red)
+        greenSlider.value = Float(green)
+        blueSlider.value = Float(blue)
     }
 }
 
